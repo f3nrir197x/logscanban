f2c6822c991492a32d4c22e048b059ca0105a6ec
Efrain B <130315497+f3nrir197x@users.noreply.github.com>
2023-07-20 16:33:22 -0400

    Update logscanban.sh
    Updated ...
    extract_ips function with:
     - output info msgs
     - add a column with the logfile where the IP was found
    ip regex to be more precise
    created a txt file on ~ with both ip addresses and paths and from there filter and create the ban file on /var/log

--- logscanban.sh
+++ logscanban.sh
@@ -1,80 +1,62 @@
 #!/bin/bash
 
 # Define a function to extract unique IP addresses from a file
+# This version outputs the 
 extract_ips() {
-    local file=$1
-    echo "Processing $file..."
-
-    # Define the command to extract IPs based on the file type
-    local cmd
-    if [[ $file == *"auth.log"* ]]; then
-        cmd="zcat -f $file | grep 'nvalid' | grep -Eo $IP_REGEX"
-    elif [[ $file == *"dovecot.log"* ]]; then
-        cmd="zcat -f $file | grep -E 'error|no\ auth' | grep -Eo $IP_REGEX"
-    elif [[ $file == *"exim4/mainlog"* ]]; then
-        cmd="zcat -f $file | grep -v 'Connection timed out' | grep -Eo $IP_REGEX"
-    elif [[ $file == *"exim4/rejectlog"* ]]; then
-        cmd="zcat -f $file | grep -Eo $IP_REGEX"
-    elif [[ $file == *"hestia/nginx-access.log"* ]]; then
-        cmd="zcat -f $file | awk '($9 !~ /^\"2/ && $9 !~ /^\"5/) {print $1}' | grep -Eo $IP_REGEX"
-    else
-        cmd="zcat -f $file | grep -Eo $IP_REGEX"
-    fi
-
-    # Run the command and append the output to the temporary file
-    local ips=$($cmd)
-    if [ $? -ne 0 ]; then
-        echo "Error running command: $cmd"
-        exit 1
-    fi
-
-    echo "$ips" | sort -u >> $TEMP_FILE
-    if [ $? -ne 0 ]; then
-        echo "Error writing to $TEMP_FILE"
-        exit 1
-    fi
-}
-
-# Function to process nginx/apache logs
-process_logs() {
-    local log_dir=$1
-    local logs=$(ls $log_dir | grep -v -E '\.error\.log')
-    if [ -n "$logs" ]; then
-        zcat -f $logs | awk '($9 !~ /^2/ && $9 !~ /^5/){print $1}' | sort -u >> $TEMP_FILE
-        if [ $? -ne 0 ]; then
-            echo "Error processing logs from $log_dir"
-            exit 1
+    local file_pattern=$1
+    for log in $file_pattern; do
+        echo "Processing $log..."
+        if [[ $log == *"auth.log"* ]]; then
+            zcat -f "$log" | grep 'nvalid' | grep -Eo $IP_REGEX | while read -r ip; do
+                echo "$ip | $log" >> $TEMP_FILE
+            done
+        elif [[ $log == *"dovecot.log"* ]]; then
+            zcat -f "$log" | grep -E 'error|no\ auth' | grep -Eo $IP_REGEX | while read -r ip; do
+                echo "$ip | $log" >> $TEMP_FILE
+            done
+        elif [[ $log == *"exim4/mainlog"* ]]; then
+            zcat -f "$log" | grep -v 'Connection timed out' | grep -Eo $IP_REGEX | while read -r ip; do
+                echo "$ip | $log" >> $TEMP_FILE
+            done
+        elif [[ $log == *"hestia/nginx-access.log"* ]]; then
+            zcat -f "$log" | awk '($9 !~ /^"2/ && $9 !~ /^"5/) {print $1}' | grep -Eo $IP_REGEX | while read -r ip; do
+                echo "$ip | $log" >> $TEMP_FILE
+            done
+        else
+            zcat -f "$log" | grep -Eo $IP_REGEX | while read -r ip; do
+                echo "$ip | $log" >> $TEMP_FILE
+            done
         fi
-    fi
+    done
 }
 
-# Define a function to handle error
-handle_error() {
-    if [ $? -ne 0 ]; then
-        echo "Error: $1"
-        exit 1
-    fi
+process_logs() {
+    local log_dir_pattern=$1
+    for log in $log_dir_pattern; do
+        zcat -f "$log" | awk '($9 !~ /^2/ && $9 !~ /^5/){print $1}' | grep -Eo $IP_REGEX | while read -r ip; do
+            echo "$ip | $log" >> $TEMP_FILE
+        done
+    done
 }
 
-IP_REGEX="([0-9]{1,3}[\.]){3}[0-9]{1,3}"
+IP_REGEX="((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
 TEMP_FILE=$(mktemp /tmp/tempfile.XXXXXX)
-handle_error "Failed to create temporary file"
-
+TRAIL=~/withpath.txt
 FILE=/var/log/meinban.txt
 EXCLUDE_IP_FILE=/var/log/exclude_ip.txt
-EXCLUDE_RANGES="AA\.BB\.CC\.|XX\.YY\.ZZ\.|RRR\.SSS\.TTT\." ### Replace the ranges with the one you want to exclude
+EXCLUDE_RANGES="XXX\.YYY\.ZZZ\.|AAA\.BBB\.CCC\.|MM\.NN\.PP\." ###Replace this with the networks to be excluded
 
 LOG_FILES=(
-    "/var/log/nginx/domains/*.error*"
-    "/var/log/nginx/error.log*"
-    "/var/log/apache2/domains/*.error*"
-    "/var/log/apache2/error.log*"
-    "/var/log/auth.log*"
-    "/var/log/dovecot.log*"
-    "/var/log/exim4/mainlog*"
-    "/var/log/exim4/rejectlog*"
-    "/var/log/hestia/nginx-access.log*"
-    "/var/log/hestia/nginx-error.log*"
+    /var/log/nginx/domains/*.error*
+    /var/log/nginx/error.log*
+    /var/log/apache2/domains/*.error*
+    /var/log/apache2/error.log*
+    /var/log/auth.log*
+    /var/log/dovecot.log*
+    /var/log/exim4/mainlog*
+    /var/log/exim4/rejectlog*
+    /var/log/hestia/nginx-access.log*
+    /var/log/hestia/nginx-error.log*
 )
 
 # Extract IPs from each log file
@@ -86,18 +68,16 @@ done
 process_logs "/var/log/nginx/domains/*.log*"
 process_logs "/var/log/apache2/domains/*.log*"
 
-# Check logs for hestia/nginx || Removed as it is already handled
-### zcat -f /var/log/hestia/nginx-access.log* | sed 's/\"//g' | awk '$9 !="200" {print $1}' | grep -Eo $IP_REGEX | sort -u >> $TEMP_FILE
-
 ### Added ban to IPs hitting ssh
-lastb | awk {'print $3'} | grep -Eo $IP_REGEX | sort -u >> $TEMP_FILE
+lastb | awk {'print $3'} | grep -Eo $IP_REGEX | while read -r ip; do
+    echo "$ip | /var/log/btmp" >> $TEMP_FILE
+done
 
-# Exclude IPs from the exclude file and the specified ranges, then sort, deduplicate and write to the final file
-grep -v -f $EXCLUDE_IP_FILE $TEMP_FILE | grep -vE $EXCLUDE_RANGES | sort -u > $FILE
-handle_error "Failed to write to $FILE"
+### Create a file with both offending IP addresses and logfile where they were found. Some duplicates may appear
+grep -v -f $EXCLUDE_IP_FILE $TEMP_FILE | grep -vE $EXCLUDE_RANGES | sort -u > $TRAIL
 
-# Remove the temporary file
+### Exclude IPs from the exclude file and the specified ranges, then sort, deduplicate and write to the final file:
+cat $TRAIL | awk -F"|" '{print $1}' | sort -u > $FILE
+
+# Remove the temporary file:
 rm $TEMP_FILE
-handle_error "Failed to remove temporary file"
-
-echo "Script completed successfully"
