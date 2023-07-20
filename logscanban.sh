33519eb7312d1f5988669e1ac1cbb4446e52fcd5
Efrain B <130315497+f3nrir197x@users.noreply.github.com>
2023-07-20 12:19:32 -0400

    Updated filters for nginx, apache2, auth.log, dovecot, exim4 and hestia

--- logscanban.sh
+++ logscanban.sh
@@ -8,26 +8,27 @@ EXCLUDE_RANGES="AA\.BB\.CC\.|XX\.YY\.ZZ\.|RRR\.SSS\.TTT\." ### Replace the range
 
 NGINX_LOGS=$(ls /var/log/nginx/domains/*.log* | grep -v -E '\.error\.log')
 if [ -n "$NGINX_LOGS" ]; then
-    zcat -f $NGINX_LOGS | awk '$9 != 200 && $9 != 500 {print $1}' | sort -u >> $TEMP_FILE
+    zcat -f $NGINX_LOGS | awk '($9 !~ /^2/ && $9 !~ /^5/) {print $1}' | sort -u >> $TEMP_FILE
 fi
 zcat -f /var/log/nginx/domains/*.error* | grep -Eo $IP_REGEX | sort -u >> $TEMP_FILE
 zcat -f /var/log/nginx/error.log* | grep -Eo $IP_REGEX | sort -u >> $TEMP_FILE
 
 APACHE2_LOGS=$(ls /var/log/apache2/domains/*.log* | grep -v -E '\.error\.log')
 if ls $APACHE2_LOGS 1> /dev/null 2>&1; then
-    zcat -f $APACHE2_LOGS | awk '$9 != 200 && $9 != 500 {print $1}' | sort -u >> $TEMP_FILE
+    zcat -f $APACHE2_LOGS | awk '($9 !~ /^2/ && $9 !~ /^5/) {print $1}' | sort -u >> $TEMP_FILE
 fi
 zcat -f /var/log/apache2/domains/*.error* | grep -Eo $IP_REGEX | sort -u >> $TEMP_FILE
 zcat -f /var/log/apache2/error.log* | grep -Eo $IP_REGEX | sort -u >> $TEMP_FILE
 
-zcat -f /var/log/auth.log* | grep -Eo $IP_REGEX | sort -u >> $TEMP_FILE
+zcat -f /var/log/auth.log* | grep 'nvalid' | grep -Eo $IP_REGEX | sort -u >> $TEMP_FILE
 
-zcat -f /var/log/dovecot.log* | grep -Eo $IP_REGEX | sort -u >> $TEMP_FILE
+zcat -f /var/log/dovecot.log* | grep -E 'error|no\auth' | grep -Eo $IP_REGEX | sort -u >> $TEMP_FILE
 
-zcat -f /var/log/exim4/mainlog* | grep -Eo $IP_REGEX | sort -u >> $TEMP_FILE
+zcat -f /var/log/exim4/mainlog* | grep -v 'Connection timed out' | grep -Eo $IP_REGEX | sort -u >> $TEMP_FILE
 zcat -f /var/log/exim4/rejectlog* | grep -Eo $IP_REGEX | sort -u >> $TEMP_FILE
 
-zcat -f /var/log/hestia/nginx-access.log* | sed 's/\"//g' | awk '$9 !="200" {print $1}' | grep -Eo $IP_REGEX | sort -u >> $TEMP_FILE
+### zcat -f /var/log/hestia/nginx-access.log* | sed 's/\"//g' | awk '$9 !="200" {print $1}' | grep -Eo $IP_REGEX | sort -u >> $TEMP_FILE
+zcat -f /var/log/hestia/nginx-access.log* | awk '($9 !~ /^\"2/ && $9 !~ /^\"5/) {print $1}' | grep -Eo $IP_REGEX | sort -u >> $TEMP_FILE
 zcat -f /var/log/hestia/nginx-error.log* | grep -Eo $IP_REGEX | sort -u >> $TEMP_FILE
 
 ### Added ban to IPs hitting ssh
